774948d753c5bea79081232c07bc9841e602ef1b
ngavinsir <ngavinsir@gmail.com>
2020-03-06 23:21:59 +0700

    headline model for model logic function

--- handlers/headline.go
+++ handlers/headline.go
@@ -1,16 +1,12 @@
 package handlers
 
 import (
-	"context"
 	"database/sql"
 	"errors"
 	"net/http"
 
 	"github.com/go-chi/render"
-	"github.com/ngavinsir/clickbait/models"
-	"github.com/segmentio/ksuid"
-	"github.com/volatiletech/sqlboiler/boil"
-	"github.com/volatiletech/sqlboiler/queries"
+	"github.com/ngavinsir/clickbait/model"
 )
 
 // RandomHeadline handler
@@ -18,7 +14,7 @@ func RandomHeadline(db *sql.DB) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		userID, _ := r.Context().Value(UserIDCtxKey).(string)
 
-		headline, err := GetRandomHeadline(r.Context(), db, userID)
+		headline, err := model.GetRandomHeadline(r.Context(), db, userID)
 		if err != nil {
 			render.Render(w, r, ErrRender(err))
 			return
@@ -37,43 +33,14 @@ func AddHeadline(db *sql.DB) http.HandlerFunc {
 			return
 		}
 
-		headline := &models.Headline{
-			ID: ksuid.New().String(),
-			Value: data.Value,
-		}
-
-		if err := headline.Insert(r.Context(), db, boil.Infer()); err != nil {
+		headline, err := model.InsertHeadline(r.Context(), db, data.Value)
+		if err != nil {
 			render.Render(w, r, ErrRender(err))
 			return
 		}
 
 		render.JSON(w, r, headline)
 	})
-}
-
-// GetRandomHeadline which labeled less than 3 times and haven't labeled by the user before
-func GetRandomHeadline(ctx context.Context, exec boil.ContextExecutor, userID string) (*Headline, error) {
-	headline := &Headline{}
-	err := queries.Raw(`
-		select h.id, h.value
-		from headlines h left join labels l on h.id = l.headline_id
-		where h.id not in (
-			select l.headline_id
-			from labels l
-			where l.user_id = $1
-			group by l.headline_id
-		)
-		group by h.id
-		having count(l.id) < $2
-		order by random()
-		limit 1
-	`, userID, 3).Bind(ctx, exec, headline)
-
-	if err != nil {
-		return &Headline{}, err
-	}
-
-	return headline, nil
 }
 
 type Headline struct {
---
+++ model/headline.go
@@ -0,0 +1,49 @@
+package model
+
+import (
+	"context"
+
+	"github.com/ngavinsir/clickbait/models"
+	"github.com/segmentio/ksuid"
+	"github.com/volatiletech/sqlboiler/boil"
+	"github.com/volatiletech/sqlboiler/queries"
+)
+
+// InsertHeadline with value
+func InsertHeadline(ctx context.Context, exec boil.ContextExecutor, value string) (*models.Headline, error) {
+	headline := &models.Headline{
+		ID: ksuid.New().String(),
+		Value: value,
+	}
+
+	if err := headline.Insert(ctx, exec, boil.Infer()); err != nil {
+		return nil, err
+	}
+
+	return headline, nil
+}
+
+// GetRandomHeadline which labeled less than 3 times and haven't labeled by the user before
+func GetRandomHeadline(ctx context.Context, exec boil.ContextExecutor, userID string) (*models.Headline, error) {
+	headline := &models.Headline{}
+	err := queries.Raw(`
+		select h.id, h.value
+		from headlines h left join labels l on h.id = l.headline_id
+		where h.id not in (
+			select l.headline_id
+			from labels l
+			where l.user_id = $1
+			group by l.headline_id
+		)
+		group by h.id
+		having count(l.id) < $2
+		order by random()
+		limit 1
+	`, userID, 3).Bind(ctx, exec, headline)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return headline, nil
+}

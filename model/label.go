78826d188b3f12c9c6bea2418cefa3f578c0ca66
ngavinsir <ngavinsir@gmail.com>
2020-07-04 14:44:41 +0700

    model/label: get label returns clickbait keywords

--- model/label.go
+++ model/label.go
@@ -7,7 +7,7 @@ import (
 	"github.com/ngavinsir/clickbait/models"
 	"github.com/segmentio/ksuid"
 	"github.com/volatiletech/sqlboiler/boil"
-	"github.com/volatiletech/sqlboiler/queries"
+	. "github.com/volatiletech/sqlboiler/queries/qm"
 )
 
 // LabelRepository handles label data management.
@@ -15,19 +15,13 @@ type LabelRepository interface {
 	InsertLabel(ctx context.Context, userID string, articleID string, value string, labelType string) (*models.Label, error)
 	IsLabeledByUser(ctx context.Context, articleID string, userID string, labelType string) (bool, error)
 	DeleteLabel(ctx context.Context, labelID string) error
-	GetArticleLabel(ctx context.Context, userID string, labelType string) ([]*ArticleLabel, error)
+	GetLabel(ctx context.Context, userID string, labelType string) ([]*models.Label, error)
 	GetArticleLabelCount(ctx context.Context, articleID string, labelType string) (int64, error)
 }
 
 // LabelDatastore holds db information.
 type LabelDatastore struct {
 	*DB
-}
-
-// ArticleLabel contains label and article
-type ArticleLabel struct {
-	models.Label   `boil:",bind" json:"label"`
-	models.Article `boil:",bind" json:"article"`
 }
 
 // InsertLabel with maximum of 3 labels per headline
@@ -89,22 +83,19 @@ func (db *LabelDatastore) DeleteLabel(ctx context.Context, labelID string) error
 	return nil
 }
 
-// GetArticleLabel return all label with the same type by user_id with the article value
-func (db *LabelDatastore) GetArticleLabel(ctx context.Context, userID string, labelType string) ([]*ArticleLabel, error) {
-	data := []*ArticleLabel{}
-	err := queries.Raw(`
-		select	l.id as "label.id", articles.id as "article.id", articles.headline as "article.headline",
-				articles.content as "article.content", l.value as "label.value",
-				l.updated_at as "label.updated_at"
-		from labels as l
-		inner join articles on l.article_id = articles.id
-		where l.user_id = $1 and l.type = $2	
-	`, userID, labelType).Bind(ctx, db, &data)
+// GetLabel return all label with the same type by user_id with the article value
+func (db *LabelDatastore) GetLabel(ctx context.Context, userID string, labelType string) ([]*models.Label, error) {
+	labels, err := models.Labels(
+		Load(models.LabelRels.Article),
+		Load(models.LabelRels.ClickbaitKeywords),
+		models.LabelWhere.UserID.EQ(userID),
+		models.LabelWhere.Type.EQ(labelType),
+	).All(ctx, db)
 	if err != nil {
 		return nil, err
 	}
 
-	return data, nil
+	return labels, nil
 }
 
 // GetArticleLabelCount returns label count by headline id.
